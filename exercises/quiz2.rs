ac3fc9720f6346bbc512ecd83576019b6bb474ff
Raivydas Šimėnas <raivydas.simenas@proton.me>
2024-05-12 18:48:17 +0300

    Do part of quiz2.rs

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -32,11 +32,16 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output: Vec<String> = vec![];
         for (string, command) in input.iter() {
             // TODO: Complete the function body. You can do it!
+            match command {
+                Uppercase => output.push(string.to_uppercase().to_string()),
+                Trim => output.push(string.trim().to_string()),
+                Append(size) => output.push(string)
+            }
         }
         output
     }
@@ -45,7 +50,7 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
